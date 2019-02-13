d8975b12797d327855313c7ad26ded37ae9a7004
Sandeep Murthy <smurthy@protonmail.ch>
2019-02-13 00:31:18 +0000

    Tweak `integerise` in `utils/utils.py` - use `isinstance` for float type checking

--- utils/utils.py
+++ utils/utils.py
@@ -183,7 +183,7 @@ def integerise(f):
         Turns a float `f` into an integer if it is an integer, otherwise
         returns the same float.
     """
-    return int(f) if type(f) == float and f.is_integer() else f
+    return int(f) if isinstance(f, float) and f.is_integer() else f
 
 
 def multinomial(n, *ks):
