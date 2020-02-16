8e96fda1a2596e964b90ad12aaaa4124b8ca0dfb
Sandeep Murthy <smurthy@protonmail.ch>
2020-02-16 18:01:50 +0000

    Tweak docstrings

--- arithmetic/digits.py
+++ arithmetic/digits.py
@@ -16,6 +16,7 @@ def digits(n, reverse=False):
     Generates the sequence of digits of a given integer ``n``, starting from
     the most significant digit, by default. If reverse is True then the
     sequence is generated from the least significant digit, e.g.
+    ::
 
         123 -> 1, 2, 3      (with no reverse or reverse=False)
         123 -> 3, 2, 1      (with reverse=True)
@@ -28,12 +29,20 @@ def digits(n, reverse=False):
 def generalised_sum(int_seq, k=1, mod=None):
     """
     Returns the sum of a sequence (or set) of integers ``int_seq`` raised to a
-    given power ``k``, reduced by a given modulus ``mod``.
-
-        [1, 2, 3]   -> 6
-        (-2, 10, 0) -> 8
-        {-1, 2, -3} -> -2
-        digits(123) -> 6
+    given power ``k``, reduced by a given modulus ``mod``, e.g.
+    ::
+        [1, 2, 3], 1, None    -> 6
+        [1, 2, 3], 2, None    -> 14
+        [1, 2, 3], 1, 5       -> 4
+        (-2, 10, 0), 1, None  -> 8
+        (-2, 10, 0), 2, None  -> 104
+        (-2, 10, 0), 2, 3     -> 2
+        {-1, 2, -3}, 1, None  -> -2
+        {-1, 2, -3}, 3, None  -> -20
+        {-1, 2, -3}, 13, None -> 2
+        digits(123), 1, None  -> 6
+        digits(123), 2, None  -> 14
+        digits(123), 2, 5     -> 4
     """
     r = reduce(lambda x, y: x + y, (n ** k for n in int_seq))
     if mod is None:
@@ -44,12 +53,20 @@ def generalised_sum(int_seq, k=1, mod=None):
 def generalised_product(int_seq, k=1, mod=None):
     """
     Returns the product of a sequence (or set) of integers ``int_seq`` raised to a
-    given power ``k``, reduced by a given modulus ``mod``.
-
-        [1, 2, 3]   -> 6
-        (-2, 10, 0) -> 0
-        {-1, 2, -3} -> 6
-        digits(123) -> 6
+    given power ``k``, reduced by a given modulus ``mod``, e.g.
+    ::
+        [1, 2, 3], 1, None    -> 6
+        [1, 2, 3], 2, None    -> 36
+        [1, 2, 3], 1, 5       -> 1
+        (-2, 10, 1), 1, None  -> -20
+        (-2, 10, 1), 2, None  -> 400
+        (-2, 10, 1), 2, 3     -> 1
+        {-1, 2, -3}, 1, None  -> 6
+        {-1, 2, -3}, 3, None  -> 216
+        {-1, 2, -3}, 13, None -> 8
+        digits(123), 1, None  -> 6
+        digits(123), 2, None  -> 36
+        digits(123), 2, 5     -> 1
     """
     r = reduce(lambda x, y: x * y, (n ** k for n in int_seq))
     if mod is None:
@@ -59,20 +76,20 @@ def generalised_product(int_seq, k=1, mod=None):
 
 def chain(*seqs):
     """
-        Generates the sequence obtained by chaining (or concatenating) a
-        sequence of sequences, e.g.
-        ::
+    Generates the sequence obtained by chaining (or concatenating) a
+    sequence of sequences, e.g.
+    ::
 
-            (1, 2), (3, 4, 5), (6, 7, 8, 9)        -> 1, 2, 3, 4, 5, 6, 7, 8, 9
+        (1, 2), (3, 4, 5), (6, 7, 8, 9)        -> 1, 2, 3, 4, 5, 6, 7, 8, 9
 
-        The arguments can be separate sequences (generators, lists, tuples) or
-        an unpacked iterable of such sequences (use * to unpack an iterable
-        argument), e.g.
-        ::
+    The arguments can be separate sequences (generators, lists, tuples) or
+    an unpacked iterable of such sequences (use * to unpack an iterable
+    argument), e.g.
+    ::
 
-            digits(12), digits(345), digits(6789)  -> 1, 2, 3, 4, 5, 6, 7, 8, 9
-            digits(12), [3, 4, 5], (6, 7, 8, 9)    -> 1, 2, 3, 4, 5, 6, 7, 8, 9
-            *[digits(12), [3, 4, 5], (6, 7, 8, 9)] -> 1, 2, 3, 4, 5, 6, 7, 8, 9
+        digits(12), digits(345), digits(6789)  -> 1, 2, 3, 4, 5, 6, 7, 8, 9
+        digits(12), [3, 4, 5], (6, 7, 8, 9)    -> 1, 2, 3, 4, 5, 6, 7, 8, 9
+        *[digits(12), [3, 4, 5], (6, 7, 8, 9)] -> 1, 2, 3, 4, 5, 6, 7, 8, 9
     """
     for c in itertools_chain(c for seq in seqs for c in seq):
         yield c
@@ -80,18 +97,19 @@ def chain(*seqs):
 
 def interlace(*seqs):
     """
-        Generates the sequence obtained by interlacing a sequence of sequences
-        (of the same length), e.g.
+    Generates the sequence obtained by interlacing a sequence of sequences
+    (of the same length), e.g.
+    ::
 
-            (1, 2, 3), (4, 5, 6), (7, 8, 9)       -> 1, 4, 7, 2, 5, 8, 3, 6, 9
+        (1, 2, 3), (4, 5, 6), (7, 8, 9)       -> 1, 4, 7, 2, 5, 8, 3, 6, 9
 
-        The arguments can be separate sequences (generators, lists, tuples) or
-        an unpacked iterable of such sequences (use * to unpack an iterable
-        argument), e.g.
+    The arguments can be separate sequences (generators, lists, tuples) or
+    an unpacked iterable of such sequences (use * to unpack an iterable
+    argument), e.g.
 
-            digits(123), digits(456), digits(789) -> 1, 4, 7, 2, 5, 8, 3, 6, 9
-            digits(123), [4, 5, 6], (7, 8, 9)     -> 1, 4, 7, 2, 5, 8, 3, 6, 9
-            *[digits(123), [4, 5, 6], (7, 8, 9)]  -> 1, 4, 7, 2, 5, 8, 3, 6, 9
+        digits(123), digits(456), digits(789) -> 1, 4, 7, 2, 5, 8, 3, 6, 9
+        digits(123), [4, 5, 6], (7, 8, 9)     -> 1, 4, 7, 2, 5, 8, 3, 6, 9
+        *[digits(123), [4, 5, 6], (7, 8, 9)]  -> 1, 4, 7, 2, 5, 8, 3, 6, 9
     """
     for e in chain(*zip(*seqs)):
         yield e
@@ -99,14 +117,15 @@ def interlace(*seqs):
 
 def int_from_digits(digits):
     """
-        Returns a positive integer ``n`` which is a decimal expansion of a
-        sequence of digits in descending order from the most significant
-        digit. The input can be a sequence (list, tuple) or a generator,
-        e.g.
+    Returns a positive integer ``n`` which is a decimal expansion of a
+    sequence of digits in descending order from the most significant
+    digit. The input can be a sequence (list, tuple) or a generator,
+    e.g.
+    ::
 
-            [1,2,3]      -> 1x10^2 + 2x10^1 + 3x10^0        =  123
-            (2, 4, 5, 1) -> 2x10^3 + 4x10^2 + 5x10 + 1x10^0 = 2451
-            digits(123)  -> 1x10^2 + 2x10^1 + 3x10^0        = 123
+        [1,2,3]      -> 1x10^2 + 2x10^1 + 3x10^0        =  123
+        (2, 4, 5, 1) -> 2x10^3 + 4x10^2 + 5x10 + 1x10^0 = 2451
+        digits(123)  -> 1x10^2 + 2x10^1 + 3x10^0        = 123
     """
     dgs = list(digits)
     n = len(dgs)
@@ -174,6 +193,8 @@ def digit_sum(n, k=1, mod=None):
     Returns the reduced digit sum of a given positive integer ``n``
     and its (additive) persistence, i.e. the number of steps taken to
     reduce ``n`` to a single digit by repeated addition of digits.
+    In each step the summation is of ``k``-th powers of digits, and
+    the sum is reduced by the given modulus ``mod``.
     """
     if n < 10:
         return n, 0
@@ -209,7 +230,9 @@ def digit_product(n, k=1, mod=None):
     """
     Returns the multiplicative digital root of a given positive integer ``n``
     and its (multiplicative) persistence, i.e. the number of steps taken to
-    reduce ``n`` to a single digit by repeating multiplication of digits.
+    reduce ``n`` to a single digit by repeated multiplication of digits.
+    In each step the multiplication is of ``k``-th powers of digits, and
+    the product is reduced by the given modulus ``mod``.
     """
     if n < 10:
         return n, 0
